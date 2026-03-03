af9303c1b2b1b73283e9c9f748d172f2f473d629
SprngVel <kraevvalentin98@gmail.com>
2026-03-03 22:09:57 +1000

    chore: обновил экспорт и убрал комментарии

--- src/indesign/utils/makeDocumentActive.ts
+++ src/indesign/utils/makeDocumentActive.ts
@@ -1,14 +1,12 @@
 import { Document } from "indesign";
 import { app } from "../../globals";
 
-export default function makeDocumentActive(document: Document): boolean {
-    if (document.isValid) {
-        app.activeDocument = document;
-        return true;
-    }
-    return false;
-    /**
-     * Попробовать проще
-     * return app.activeDocument.name === document.name
-     */
+/**
+ * @description делает документ активным (у пользователя вкладка документа становится активной). При отсутствии документа возвращает false
+ */
+export function makeDocumentActive(document: Document): boolean {
+    if (!document.isValid) return false;
+
+    app.activeDocument = document;
+    return true;
 }
