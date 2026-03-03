6e6abe0a5e4b512d1eb10581d5483dbdf066161f
SprngVel <kraevvalentin98@gmail.com>
2026-03-03 23:04:45 +1000

    feat: добавлен компонент для ErrorBoundary

--- src/plugin/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/plugin/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,32 +1,59 @@
+import { Component } from "react";
+import "./ErrorBoundary.scss";
+
 interface ErrorBoundaryProps {
-    fallback: React.ReactNode;
-    children: React.ReactNode;
+    hasError: boolean;
+    error: any;
 }
 
-export default class ErrorBoundary extends React.Component<
-    ErrorBoundaryProps,
-    { hasError: boolean }
-> {
+export class ErrorBoundary extends Component<any, ErrorBoundaryProps> {
     constructor(props: ErrorBoundaryProps) {
         super(props);
-        this.state = { hasError: false };
+        this.state = { hasError: false, error: null };
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    static getDerivedStateFromError(_: Error) {
-        return { hasError: true };
+    static getDerivedStateFromError(error: Error) {
+        return { hasError: true, error: error };
     }
 
-    componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
-        console.log(error, errorInfo);
+    public componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+        console.error(error, errorInfo);
     }
 
-    render() {
+    public render() {
         if (this.state.hasError) {
-            return this.props.fallback;
+            const { error } = this.state;
+
+            return (
+                <main className="error-boundary">
+                    <section className="error-boundary__inner-wrapper">
+                        <h2>Произошла непредвиденная ошибка</h2>
+                        <p className="muted">
+                            Если ошибка повторяется, пришлите, пожалуйста, информацию об ошибке на
+                            данный электронный адрес:{" "}
+                            <a href="mailto:kraevvalentin98@gmail.com">kraevvalentin98@gmail.com</a>
+                        </p>
+                        <section className="error-info">
+                            <p className="muted">Информация об ошибке</p>
+                            <p className="error-info--red">Ошибка: {error.message}</p>
+                            {error?.cause && (
+                                <p className="error-info--red">Причина: {error.cause}</p>
+                            )}
+                        </section>
+                        <div className="error-boundary__btn-wrapper">
+                            <sp-button
+                                onClick={(event: MouseEvent) => {
+                                    event.preventDefault();
+                                    location.reload();
+                                }}
+                            >
+                                Попробовать снова
+                            </sp-button>
+                        </div>
+                    </section>
+                </main>
+            );
         }
         return this.props.children;
     }
 }
-
-// export default ErrorBoundary
