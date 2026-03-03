d459d925432110c5b626221454b0acee20664670
SprngVel <kraevvalentin98@gmail.com>
2026-03-03 23:23:38 +1000

    chore: удалены упоминания старых файлов

--- src/indesign/constants/stylesNames.ts
+++
@@ -1,5 +0,0 @@
-export const STYLES_NAMES = {
-    CHARACTER_STYLE_GROUP: "Plugin (highlight)",
-    COLOR_GROUP: "Plugin (colors)",
-    ACTIVE: "activeStyle",
-};
--- src/litera5/config.ts
+++
@@ -1,6 +0,0 @@
-import { Litera5ApiConfig } from "litera5-api-js-client/lib/Litera5Api";
-
-export const config: Litera5ApiConfig = {
-    company: "", // Название Вашей компании,
-    secret: "", // Ваш API-ключ от Литеры
-};
