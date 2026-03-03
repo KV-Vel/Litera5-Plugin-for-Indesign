c264cd38833ef9c8a979c036218eabf54935e0ea
SprngVel <kraevvalentin98@gmail.com>
2026-03-03 22:03:25 +1000

    chore: обновил экспорт и комментарии

--- src/indesign/utils/getSelection.ts
+++ src/indesign/utils/getSelection.ts
@@ -3,11 +3,15 @@ import { app } from "../../globals";
 /**
  * @description возвращает выбранный пользователем объект в Indesign
  */
-export default function getSelection<T extends { constructorName: string }>(): T | undefined {
+export function getSelection<T extends { constructorName: string }>(): T | undefined {
     if (!app.documents.length) {
         return undefined;
     }
-    // Никогда не видел, чтобы app.selection возвращало не массив, поэтому приводим к типу
+    /**
+     * В некоторых местах типы Indesign не совпадает с тем, как есть на самом деле.
+     * И я никогда не видел, чтобы app.selection возвращало не массив, поэтому приводим к типу.
+     */
+
     const selection = app.selection as object[];
     const hasSelectedItem = selection.length > 0;
     if (hasSelectedItem) {
