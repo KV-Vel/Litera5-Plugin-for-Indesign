839f5587137051e5e39fd041d0d79410068f6049
SprngVel <kraevvalentin98@gmail.com>
2026-03-03 23:20:38 +1000

    refactor: нижний компонент страницы теперь предназначен также для отображения ошибок + др. правки

--- src/plugin/pages/EditorsPage/components/BottomActionBar/BottomActionBar.tsx
+++ src/plugin/pages/EditorsPage/components/BottomActionBar/BottomActionBar.tsx
@@ -1,48 +1,48 @@
-import { OrthoKind } from "litera5-api-js-client";
 import { RequestProps } from "../../../AuthPage/types";
 import useTextCheck from "../../../../hooks/useTextCheck";
-import { EXCEPTIONS_DEFAULT_STATE, EXCEPTIONS_NAME } from "../../../../constants/exceptionsDefault";
-import { indesignUtils } from "../../../../../indesign/utils";
-import capitalize from "../../../../utils/capitalize";
+import "./BottomActionBar.scss";
+import { getUserSettings, capitalize } from "../../../../utils";
+import { getSelection } from "../../../../../indesign/utils";
 import { Alert, Loader } from "../../../../components";
 import { AlertVariant } from "../../../../components/Alert/types";
-import "./BottomActionBar.scss";
 
 interface BottomActionBarProps extends RequestProps {
-    handleClearAnnotations: () => void;
+    сlearAnnotations: () => void;
 }
 
 export function BottomActionBar({
     login,
     onRequest,
-    handleClearAnnotations,
-}: BottomActionBarProps) {
+    сlearAnnotations,
+    children,
+}: React.PropsWithChildren<BottomActionBarProps>) {
     const [isLoading, progress, errorState, clearError, handleTextCheck] = useTextCheck();
 
     function startNewTextCheck(login: string) {
-        const exceptions = localStorage.getItem(EXCEPTIONS_NAME);
-        const parsedExceptions: Record<OrthoKind, boolean> = exceptions
-            ? JSON.parse(exceptions)
-            : EXCEPTIONS_DEFAULT_STATE;
+        // Дефолтное value прокидывается во время первой проверки, при повторной проверке оно уже будет в сторедже.
+        const userSettings = getUserSettings();
 
-        onRequest(() =>
-            handleTextCheck(login.trim(), indesignUtils.getSelection(), parsedExceptions),
-        );
+        onRequest(() => handleTextCheck(login.trim(), getSelection(), userSettings));
     }
+
     return (
         <div className="bottom-action-bar">
             {isLoading && (
                 <Loader currentValue={progress.progress} message={capitalize(progress.message)} />
             )}
-            {errorState.hasError && (
+            {errorState && (
                 <Alert
                     header="Ошибка"
-                    description={errorState.message}
+                    description={errorState}
                     type={AlertVariant.WARNING}
                     onClose={clearError}
                 />
             )}
-            <sp-button onClick={handleClearAnnotations} variant="secondary">
+            {/**
+             * children необходим для прокидывания других ошибок, т.к - это единственное место, где удобно их отобразить.
+             */}
+            {children}
+            <sp-button onClick={сlearAnnotations} variant="secondary">
                 Очистить подсказки
             </sp-button>
             <sp-button disabled={isLoading} onClick={() => startNewTextCheck(login)}>
