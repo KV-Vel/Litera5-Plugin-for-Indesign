2d71931899e7bc6473ff76562f706473c415aef1
SprngVel <kraevvalentin98@gmail.com>
2026-03-03 23:11:22 +1000

    fix: теперь страница SettingsPage не будет отображаться вместо AuthPage. AuthPage будет получать display: none

--- src/plugin/pages/AuthPage/AuthPage.tsx
+++ src/plugin/pages/AuthPage/AuthPage.tsx
@@ -18,84 +18,72 @@ export default function AuthPage({ login, onLoginChange, onRequest }: AuthPagePr
 
     return (
         <div className="container-column">
-            {settingsPageIsActive ? (
+            {settingsPageIsActive && (
                 <SettingsPage onReturn={() => setSettingsPageIsActive(false)} />
-            ) : (
-                <>
-                    <TopActionBar>
-                        <div className="authpage__header">
-                            {/* <Logo /> */}
-                            <div
-                                onClick={() => setSettingsPageIsActive(true)}
-                                className="settings-btn"
-                            >
-                                <img
-                                    src={cog}
-                                    alt="Иконка меню настроек"
-                                    className="settings-btn__icon"
-                                />
-                            </div>
-                        </div>
-                    </TopActionBar>
+            )}
+            <TopActionBar>
+                <div className="authpage__header">
+                    {/* <Logo /> */}
+                    <div onClick={() => setSettingsPageIsActive(true)} className="settings-btn">
+                        <img src={cog} alt="Иконка меню настроек" className="settings-btn__icon" />
+                    </div>
+                </div>
+            </TopActionBar>
 
-                    <div className="authpage__form-wrapper">
-                        <div className="authpage__inner-form-wrapper">
-                            <div className="authpage__description">
-                                <h2>Начало проверки</h2>
+            <div className={`authpage__form-wrapper ${settingsPageIsActive ? "hidden" : ""}`}>
+                <div className="authpage__inner-form-wrapper">
+                    <div className="authpage__description">
+                        <h2>Начало проверки</h2>
 
-                                <ol>
-                                    <li>
-                                        <span>1</span>
-                                        <article>
-                                            <h4>Введите логин</h4>
-                                            <p>
-                                                Логин указывается в формате invanov.av, где{" "}
-                                                <strong>ivanov</strong> — фамилия, а{" "}
-                                                <strong>av</strong> — инициалы.
-                                            </p>
-                                        </article>
-                                    </li>
-                                    <li>
-                                        <span>2</span>
-                                        <article>
-                                            <h4>Выберите текст для проверки</h4>
-                                            <p>Выделите текст во фрейме или сам фрейм.</p>
-                                        </article>
-                                    </li>
-                                    <li>
-                                        <span>3</span>
-                                        <article>
-                                            <h4>
-                                                Работайте с привычным Вам интерфейсом, прямиком в
-                                                Indesign!
-                                            </h4>
-                                            <p>
-                                                Нажмите кнопку &quot;Проверить текст&quot; и
-                                                дождитесь окончания проверки,{" "}
-                                                <strong>не снимая выделение</strong>.
-                                            </p>
-                                        </article>
-                                    </li>
-                                </ol>
-                                <div className="alert-wrapper">
-                                    <Alert
-                                        header="Примечание"
-                                        type={AlertVariant.INFO}
-                                        description="Проверка будет выполнена от лица пользователя, указанного в поле ниже.
+                        <ol>
+                            <li>
+                                <span>1</span>
+                                <article>
+                                    <h4>Введите логин</h4>
+                                    <p>
+                                        Логин указывается в формате invanov.av, где{" "}
+                                        <strong>ivanov</strong> — фамилия, а <strong>av</strong> —
+                                        инициалы.
+                                    </p>
+                                </article>
+                            </li>
+                            <li>
+                                <span>2</span>
+                                <article>
+                                    <h4>Выберите текст для проверки</h4>
+                                    <p>Выделите текст во фрейме или сам фрейм.</p>
+                                </article>
+                            </li>
+                            <li>
+                                <span>3</span>
+                                <article>
+                                    <h4>
+                                        Работайте с привычным Вам интерфейсом, прямиком в Indesign!
+                                    </h4>
+                                    <p>
+                                        Нажмите кнопку &quot;Проверить текст&quot; и дождитесь
+                                        окончания проверки, <strong>не снимая выделение</strong>.
+                                    </p>
+                                </article>
+                            </li>
+                        </ol>
+                        <div className="alert-wrapper">
+                            <Alert
+                                header="Примечание"
+                                type={AlertVariant.INFO}
+                                description="Проверка будет выполнена от лица пользователя, указанного в поле ниже.
                                 Проверенный текст также будет доступен для просмотра в личном кабинете на сайте Литеры."
-                                    />
-                                </div>
-                            </div>
-
-                            <RequestCheckForm
-                                login={login}
-                                onLoginChange={onLoginChange}
-                                onRequest={onRequest}
                             />
                         </div>
                     </div>
-                </>
-            )}
+
+                    <RequestCheckForm
+                        login={login}
+                        onLoginChange={onLoginChange}
+                        onRequest={onRequest}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
