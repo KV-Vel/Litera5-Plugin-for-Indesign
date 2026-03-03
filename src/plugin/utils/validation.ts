1ee459478f85e6f0c73ad977ef5c0b0c838d9c07
SprngVel <kraevvalentin98@gmail.com>
2026-03-03 22:40:42 +1000

    refactor: переделал выкидывание ошибок

--- src/plugin/utils/validation.ts
+++ src/plugin/utils/validation.ts
@@ -1,64 +1,52 @@
 import { TextFrame } from "indesign";
 import { TextVariations } from "../../types/data";
 import { LITERA5_MIN_TEXT_LENGTH, LITERA5_LOGIN_PATTERN } from "../../litera5/constants";
+import { getSelection } from "../../indesign/utils";
 
 export function loginIsValid(login: string) {
     if (!login.trim().length) {
-        return { valid: false, errorMessage: "Не указан логин." };
+        throw new Error("Не указан логин.");
     }
 
     const loginHasValidPattern = LITERA5_LOGIN_PATTERN.test(login.trim());
     if (!loginHasValidPattern) {
-        return {
-            valid: false,
-            errorMessage:
-                "Логин не соответствует формату. Правильный формат: ivanov.av, где invanov — фамилия, а av — инициалы.",
-        };
+        throw new Error(
+            "Логин не соответствует формату. Правильный формат: ivanov.av, где invanov — фамилия, а av — инициалы.",
+        );
     }
 
-    return { valid: true };
+    return true;
 }
 
-export function indesignSelectionIsValid<
-    T extends { constructorName: string } | TextFrame | TextVariations | undefined,
->(selection: T) {
+export function indesignSelectionIsValid(selection: ReturnType<typeof getSelection>) {
     if (!selection) {
-        return { valid: false, errorMessage: "Не найден выделенный текст или фрейм." };
+        throw new Error("Не найден выделенный текст или фрейм.");
     }
 
     if (selection.constructorName === "Cell") {
-        return {
-            valid: false,
-            errorMessage: "Нельзя проверить текст в таблице.",
-        };
+        throw new Error("Плагин не поддерживает проверку текста в таблице.");
     }
 
     const selectionWithoutTexts = !("texts" in selection);
     if (selectionWithoutTexts) {
-        return {
-            valid: false,
-            errorMessage:
-                "Не найден текст в выделенном объекте. Убедитесь, что Вы выделили фрейм или текст в нем.",
-        };
+        throw new Error(
+            "Не найден текст в выделенном объекте. Убедитесь, что Вы выделили фрейм или текст в нем.",
+        );
     }
 
     if ((selection as TextFrame).overflows) {
-        return {
-            valid: false,
-            errorMessage:
-                "В выделенном объекте обнаружен вытесненный текст. Удалите вытесненный текст или переместите его в другой фрейм.",
-        };
+        throw new Error(
+            "В выделенном объекте обнаружен вытесненный текст. Удалите вытесненный текст или переместите его в другой фрейм.",
+        );
     }
 
     const selectionReachMinTextLength =
         (selection as TextVariations).texts.firstItem().length > LITERA5_MIN_TEXT_LENGTH;
     if (!selectionReachMinTextLength) {
-        return {
-            valid: false,
-            errorMessage:
-                "Объём проверяемого текста должен быть не меньше 32 и не больше 100.000 знаков.",
-        };
+        throw new Error(
+            "Объём проверяемого текста должен быть не меньше 32 и не больше 100.000 знаков.",
+        );
     }
 
-    return { valid: true };
+    return true;
 }
