e8bc8fe8fab6c182b0e94e89d8d010f8f6411d0d
SprngVel <kraevvalentin98@gmail.com>
2026-07-28 21:20:09 +1000

    feat: файл реэкспорта для хуков

---
+++ src/plugin/hooks/index.ts
@@ -0,0 +1,5 @@
+export * from "./useInddKindsHighlight";
+export * from "./useLocalStorage";
+export * from "./useSecureStorage";
+export * from "./useTextCheck";
+export * from "./useWithDocumentOpen";
