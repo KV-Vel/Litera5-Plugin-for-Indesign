d3014284604f550d5bdd419d15e54b796b3dc366
SprngVel <kraevvalentin98@gmail.com>
2026-07-28 21:08:28 +1000

    refactor: добавил getSecureStorageData для реэкспорта

--- src/plugin/utils/index.ts
+++ src/plugin/utils/index.ts
@@ -2,3 +2,4 @@ export * from "./capitalize";
 export * from "./createAppDataFromResponse";
 export * from "./getUserSettings";
 export * from "./validation";
+export * from "./getSecureStorageData";
